783b968ed2c6a6318896589d108de1141d434939
GhenadieVP <ghenadie@radixpublishing.com>
2025-04-10 11:47:22 +0300

    Remove Rx

--- Package.swift
+++ Package.swift
@@ -12,13 +12,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "ScreenshotPreventing", targets: ["ScreenshotPreventing"]),
         .library(name: "ScreenshotPreventingSwiftUI", targets: ["ScreenshotPreventingSwiftUI"]),
-        .library(name: "RxScreenshotPreventing", targets: ["RxScreenshotPreventing"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -31,15 +26,6 @@ let package = Package(
             name: "ScreenshotPreventingSwiftUI",
             dependencies: ["ScreenshotPreventing"],
             path: "Sources/SwiftUI"
-        ),
-        .target(
-            name: "RxScreenshotPreventing",
-            dependencies: [
-                "ScreenshotPreventing",
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "RxCocoa", package: "RxSwift"),
-            ],
-            path: "Sources/RxSwift"
         ),
         .testTarget(
             name: "ScreenshotPreventingTests",
